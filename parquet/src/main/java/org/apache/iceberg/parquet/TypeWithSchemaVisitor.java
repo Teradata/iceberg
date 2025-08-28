ddf18abe8e30dfa18cd0cfafd29b7dc576ba90e5
Lavanya  Keshetty <lk255018@teradata.com>
2025-08-28 10:22:09 -0700

    Fix struct fieldid if missing in fileschema, read from expected(which is projected schema.

--- parquet/src/main/java/org/apache/iceberg/parquet/TypeWithSchemaVisitor.java
+++ parquet/src/main/java/org/apache/iceberg/parquet/TypeWithSchemaVisitor.java
@@ -198,11 +198,12 @@ public class TypeWithSchemaVisitor<T> {
   private static <T> List<T> visitFields(
       Types.StructType struct, GroupType group, TypeWithSchemaVisitor<T> visitor) {
     List<T> results = Lists.newArrayListWithExpectedSize(group.getFieldCount());
+    int i =0;
     for (Type field : group.getFields()) {
-      int id = -1;
-      if (field.getId() != null) {
-        id = field.getId().intValue();
-      }
+      int id = field.getId() != null
+              ? field.getId().intValue()
+              : (struct != null) ? struct.fields().get(i).fieldId() : -1;
+      i++;
       Types.NestedField iField = (struct != null && id >= 0) ? struct.field(id) : null;
       results.add(visitField(iField, field, visitor));
     }
